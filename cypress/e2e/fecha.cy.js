107ffa25820934c81f57634ab59504ad4924dcdc
institutoweb <137358271+institutoweb@users.noreply.github.com>
2023-11-21 10:40:42 -0300

    Update fecha.cy.js

--- cypress/e2e/fecha.cy.js
+++ cypress/e2e/fecha.cy.js
@@ -7,13 +7,13 @@ describe('Date Picker Test', () => {
       // Hacer clic en el campo de entrada del date picker
       cy.get('.datepicker-input').click();
   
-      // Seleccionar la fecha 2023-07-14 en el date picker
+      // Seleccionar la fecha 2023-11-14 en el date picker
       cy.get('.datepicker-dropdown')
       .find('.datepicker-days tbody tr:nth-child(3) td:nth-child(3)')
       .click();
   
       // Verificar que la fecha seleccionada se haya establecido correctamente en el campo de entrada
-      cy.get('.datepicker-input').should('have.value', '2023-11-10');
+      cy.get('.datepicker-input').should('have.value', '2023-11-14');
     });
   });
   
